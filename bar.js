6a162cb180d4d22d99091f76cd27acff64d4d36d
GorgeousStar <innocentblesel@gmail.com>
2022-11-07 08:09:48 +0100

    I created a sidebar with html,css and javascript

---
+++ bar.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "50%";
+    document.getElementById("main").style.display = "block"
+  }
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
